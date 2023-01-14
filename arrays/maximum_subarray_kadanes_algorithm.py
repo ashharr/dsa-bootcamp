5976090abb902c59cf6e5166a48fbf6f1b9c0842
ashharr <shaikh.ashhar6@gmail.com>
2023-01-14 18:20:13 +0530

    arrays

--- arrays/maximum_subarray_kadanes_algorithm.py
+++ arrays/maximum_subarray_kadanes_algorithm.py
@@ -1,6 +1,7 @@
+# Maximum Subarray
 # Given an integer array nums, find the 
-# subarray
-#  with the largest sum, and return its sum.
+# subarray with the largest sum, and return its sum.
+
 # Example 1:
 
 # Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
