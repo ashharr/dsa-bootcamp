0b994434d0f520c38bd6a26c9db4141eedc4e383
Ashhar Shaikh <46366342+ashharr@users.noreply.github.com>
2023-02-28 13:29:56 +0530

    stacks - added

---
+++ stack/Stack.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+class Stack {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(35);
+        stack.push(12);
+        stack.push(66);
+        stack.push(12);
+        stack.push(66);
+        
+
+    }
+}
