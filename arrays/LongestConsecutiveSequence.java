48e0d62b8a6c3b99160d7eee421e374977c74268
Ashhar Shaikh <46366342+ashharr@users.noreply.github.com>
2023-03-14 13:42:31 +0530

    array java problems added

---
+++ arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,21 @@
+class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        HashSet s = new HashSet<>();
+        for(int i: nums){
+            s.add(i);
+        }
+        int maxLen = 0;
+        for(int num: nums){
+            if(!s.contains(num - 1)){
+                int currNum = num;
+                int currLen = 1;
+                while( s.contains(currNum + 1)){
+                    currNum += 1;
+                    currLen += 1;
+                }
+                maxLen = Math.max(maxLen, currLen);
+            }
+        }
+        return maxLen;
+    }
+}
