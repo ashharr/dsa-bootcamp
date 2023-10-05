ea9c70dae5a23628559b9b9a6b4a20c3202548ed
ashharr <shaikh.ashhar6@gmail.com>
2023-10-05 19:36:21 +0530

    best time to buy stock

---
+++ arrays/BestTimeToBuySellStock.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+
+class BestTimeToBuySellStock {
+    public static int maxProfit(int[] prices) {
+        int low = prices[0], maxProfit = 0;
+		for(int i = 1; i < prices.length ; i++) {
+			if(prices[i] < low) low = prices[i];
+			int temp = prices[i] - low;
+			maxProfit = Math.max(maxProfit, temp);
+		}
+		return maxProfit;
+    }
+	public static void main(String[] args) {
+		System.out.println(maxProfit(new int[] {7,1,5,3,6,4}));
+		
+	}
+}
